194a7945f60f095037a88f32158ce519921c2dd6
albert <albert@alberts-Mini.fritz.box>
2017-10-09 12:15:06 +0200

    tested successfully restore

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
+# Prereq mac
+install brew
+```bash
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install coreutils
+PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+```
+
 # How to restore gitlab
 
-Restore docker-volume directory from backup in /docker-volumes. E.g.:
+Create place where the docker stuff will be available. E.g.:
 
 Create directories:
 ```bash
@@ -10,9 +18,11 @@ mkdir $docker_installation_dir/docker-volumes
 su ln -s $docker_installation_dir/docker-volumes /docker-volumes
 ```
 
-Now populate /docker-volumes with all the relevant files from the backup (we don't need gitlab dir).
+You need to add this dir in the configuration of docker to be allowed for sharing 
+(see e.g. https://stackoverflow.com/questions/45122459/docker-mounts-denied-the-paths-are-not-shared-from-os-x-and-are-not-known).
 
-Afterwards we can use the setup-gitlab.sh script to restore gitlab from the backup.
+Afterwards we can use the setup-gitlab.sh script to restore gitlab from the backup (in case backup is in a different folder than expected
+you need to change it in the script as this script restores the docker-volumes directory backup).
 
 ```bash
 git clone git@github.com:albert0815/gitlab.git
--- setup-gitlab.sh
+++ setup-gitlab.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
-
+set -e
 echo "starting installation of gitlab"
+
+echo "removing old stuff"
+
+backup_dir=/Volumes/FRITZ.NAS/PI-239-USB2-0Drive-01/gitlab-backup
+
+
+rm -rf /docker-volumes/*
+cp -rf $backup_dir/docker-volumes/* /docker-volumes
+ln -s $backup_dir /docker-volumes/backup
+docker stop gitlab && docker rm gitlab
+
 
 echo "starting gitlab docker image"
 docker run --detach \
     --hostname alberts-mini.fritz.box \
-    --env GITLAB_OMNIBUS_CONFIG="external_url 'http://http://alberts-mini.fritz.box:60700'; gitlab_rails['gitlab_shell_ssh_port'] = 60722" \
+    --env GITLAB_OMNIBUS_CONFIG="external_url 'http://alberts-mini.fritz.box:60700'; gitlab_rails['gitlab_shell_ssh_port'] = 60722" \
     --publish 60700:60700 --publish 60722:22 \
     --name gitlab \
     --restart always \
@@ -13,29 +24,38 @@ docker run --detach \
     --volume /docker-volumes/gitlab/logs:/var/log/gitlab \
     --volume /docker-volumes/gitlab/data:/var/opt/gitlab \
     --volume /docker-volumes/backup:/backup\
-    gitlab/gitlab-ce:10.0.3
+    gitlab/gitlab-ce:10.0.3-ce.0
 
-git clone https://github.com/vishnubob/wait-for-it
-wait-for-it/wait-for-it.sh localhost:60700
+echo "checking status of gitlab and waiting until it is healthy"
+status=`docker inspect --format='{{.State.Health.Status}}' gitlab`
+while [ "$status" != "healthy" ] 
+do
+	echo "status is $status, waiting another 5 secs"
+	sleep 5
+	status=`docker inspect --format='{{.State.Health.Status}}' gitlab`
+done
 
-backup_dir=/docker-volumes/backup/gitlab/applications
-backup_file=`ls -t backup_dir|head -1`
-echo "using backup $backup_dir$backup_file"
-cp $backup_dir$backup_file /docker-volumes/gitlab/data/data/backups
+echo "status is now $status, we will now restore the applications"
+
+gitlab_backup_dir=/docker-volumes/backup/gitlab
+backup_file=`ls -t $gitlab_backup_dir|head -1`
+cp $gitlab_backup_dir/$backup_file /docker-volumes/gitlab/data/backups
+echo "using backup $gitlab_backup_dir$backup_file"
 
 echo "stopping unicorn and sidekiq"
-docker exec -it gitlab_web_1 gitlab-ctl stop unicorn
-docker exec -it gitlab_web_1 gitlab-ctl stop sidekiq 
+docker exec -it gitlab gitlab-ctl stop unicorn
+docker exec -it gitlab gitlab-ctl stop sidekiq 
 
-echo "executing restore"
-docker exec -it gitlab_web_1 gitlab-rake gitlab:backup:restore BACKUP=`$backup_file|cut -c1-28`
+backup_version=`echo $backup_file|cut -c1-28`
+echo "executing restore with version $backup_version"
+docker exec -it gitlab gitlab-rake gitlab:backup:restore BACKUP=$backup_version
 echo "executing starting gitlab"
-docker exec -it gitlab_web_1 gitlab-ctl start
+docker exec -it gitlab gitlab-ctl start
 
 echo "starting runner image"
 docker run -d --name gitlab-runner --restart always \
   -v /docker-volumes/gitlab-runner/config:/etc/gitlab-runner \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  gitlab/gitlab-runner:10.0.2
+  gitlab/gitlab-runner:v10.0.2
   
 echo "you need to register the runner now: docker exec -it gitlab-runner gitlab-runner register. after registration you need to change volumes config of the runner."
