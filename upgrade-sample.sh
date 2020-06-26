ffbd7dde7870b0a73cfcbb6f5b95560b30be8aa4
albert0815 <mail@dirkpapenberg.de>
2020-06-26 23:04:46 +0200

    Update upgrade-sample.sh

--- upgrade-sample.sh
+++ upgrade-sample.sh
@@ -11,7 +11,7 @@ docker stop gitlab && docker rm gitlab
 docker stop gitlab-runner && docker rm gitlab-runner
 
 # put in correct version in below docker images, make sure to go through the upgrade guide as per https://docs.gitlab.com/ee/policy/maintenance.html#upgrade-recommendations
-
+# do not update to 13.1.1 due to https://gitlab.com/gitlab-org/omnibus-gitlab/-/issues/5468 !
 docker run --detach \
     --publish 60722:22 \
     --publish 60780:60780 \
@@ -21,13 +21,13 @@ docker run --detach \
     --volume /docker-volumes/gitlab/logs:/var/log/gitlab \
     --volume /docker-volumes/gitlab/data:/var/opt/gitlab \
     --volume /docker-volumes/backup:/backup\
-    gitlab/gitlab-ce:13.1.1-ce.0
+    gitlab/gitlab-ce:12.10.6-ce.0
 
 	
 docker run -d --name gitlab-runner --restart always \
   -v /docker-volumes/gitlab-runner/config:/etc/gitlab-runner \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  gitlab/gitlab-runner:v13.1.0
+  gitlab/gitlab-runner:v12.10.3
 
   
 docker network connect gitlab-network gitlab-runner || true
